28cb20754dd8f951529658cb71f4d9302d4a67ab
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-13 09:04:02 -0300

    adicionado style para title

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -1,12 +1,11 @@
 import React from "react";
-import {View, Text} from "react-native"
+import { View, Text } from "react-native";
+import styles from "./style";
 
 export default function Title() {
-    return(
-        <View>
-            <Text>
-                Cáculo IMC
-            </Text>
-        </View>
-    );
+  return (
+    <View style={styles.boxTitle}>
+      <Text style={styles.textTitle}>Cáculo IMC</Text>
+    </View>
+  );
 }
