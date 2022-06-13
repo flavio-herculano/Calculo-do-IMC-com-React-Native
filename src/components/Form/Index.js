761303a76cb120a45743486362302a5b0c8ae76b
Flávio Herculano <flavio.hercullano@gmail.com>
2022-06-13 10:30:04 -0300

    adicionado style para o form

--- src/components/Form/Index.js
+++ src/components/Form/Index.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, TextInput, View, Text } from "react-native";
+import { TouchableOpacity, TextInput, View, Text } from "react-native";
 import ResultImc from "./ResultImc";
 import styles from "./style";
 
@@ -29,23 +29,30 @@ export default function Form() {
   }
 
   return (
-    <View>
-      <View>
-        <Text>Altura</Text>
+    <View style={styles.formContext}>
+      <View style={styles.form}>
+        <Text style={styles.formLabel}>Altura</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setHeight}
           value={height}
           placeholder="Ex. 1.60"
           keyboardType="number-pad"
         />
-        <Text>Peso</Text>
+        <Text style={styles.formLabel}>Peso</Text>
         <TextInput
+          style={styles.input}
           onChangeText={setWeight}
           value={weight}
           placeholder="Ex. 60.176"
           keyboardType="number-pad"
         />
-        <Button onPress={() => validationImc()} title={textButton} />
+        <TouchableOpacity
+          style={styles.buttonCalculator}
+          onPress={() => validationImc()}
+        >
+          <Text style={styles.textButtonCalculator}>{textButton}</Text>
+        </TouchableOpacity>
       </View>
       <ResultImc messageResultImc={messageImc} resultImc={imc} />
     </View>
